444a05f41a88f354063afd5ab42ec4331f1cbc45
Bigfootmandude <davidjkeizer@gmail.com>
2017-11-12 21:28:55 -0700

    Further Javadoc and documentation

--- CoinReceptacleListenerDevice.java
+++ CoinReceptacleListenerDevice.java
@@ -11,24 +11,40 @@ import org.lsmr.vending.hardware.CoinReceptacleListener;
 public class CoinReceptacleListenerDevice implements CoinReceptacleListener{
 
 	private VendingLogicInterface logic;
+	//TODO Why are these public? Should these not be private?
 	public int enabledCount = 0;
 	public int disabledCount = 0;
 	public int coinValue = 0;
 	public int coinCount = 0;
 	public boolean receptaclesFull = false;
 	
+	
+	/**
+	*
+	* Constructor creates the listener, and assigns a logic interface to interact with
+	* @param
+	*/
 	public CoinReceptacleListenerDevice(VendingLogicInterface logic)
 	{
 		this.logic = logic;
 	}
 	
-	
+	/**
+	* Method enables a specific piece of harware 
+	* @param None
+	* @return None
+	*/
 	@Override
 	public void enabled(AbstractHardware<? extends AbstractHardwareListener> hardware) {
 	    enabledCount++;
 	    logic.enableHardware(hardware);
 	}
 
+	/**
+	* Disables a specific piece of hardware
+	* @param None
+	* @return None
+	*/
 	@Override
 	public void disabled(AbstractHardware<? extends AbstractHardwareListener> hardware) {
 	    disabledCount++;
@@ -36,14 +52,14 @@ public class CoinReceptacleListenerDevice implements CoinReceptacleListener{
 	}
 
 	/**
-     * An event that announces that the indicated coin has been added to the
-     * indicated receptacle.
-     * 
-     * @param receptacle
-     *            The receptacle where the event happened.
-     * @param coin
-     *            The coin added.
-     */
+     	* An event that announces that the indicated coin has been added to the
+    	* indicated receptacle.
+     	* 
+     	* @param receptacle
+     	*            The receptacle where the event happened.
+     	* @param coin
+     	*            The coin added.
+     	*/
 	@Override
 	public void coinAdded(CoinReceptacle receptacle, Coin coin) {	
 		coinValue += coin.getValue();
@@ -53,12 +69,12 @@ public class CoinReceptacleListenerDevice implements CoinReceptacleListener{
 	}
 
 	/**
-     * An event that announces that all coins have been removed from the
-     * indicated receptacle.
-     * 
-     * @param receptacle
-     *            The receptacle where the event happened.
-     */
+     	* An event that announces that all coins have been removed from the
+     	* indicated receptacle.
+     	* 
+     	* @param receptacle
+     	*            The receptacle where the event happened.
+     	*/
 	@Override
 	public void coinsRemoved(CoinReceptacle receptacle) {
 		coinValue = 0;
@@ -68,11 +84,11 @@ public class CoinReceptacleListenerDevice implements CoinReceptacleListener{
 	}
 
 	/**
-     * An event that announces that the indicated receptacle is now full.
-     * 
-     * @param receptacle
-     *            The receptacle where the event happened.
-     */
+    	* An event that announces that the indicated receptacle is now full.
+     	* 
+     	* @param receptacle
+     	*            The receptacle where the event happened.
+     	*/
 	@Override
 	public void coinsFull(CoinReceptacle receptacle) {
 		if(receptacle.getCapacity()<=receptacle.size()) {
@@ -83,15 +99,15 @@ public class CoinReceptacleListenerDevice implements CoinReceptacleListener{
 	}
 
 	/**
-     * Announces that the indicated sequence of coins has been added to the
-     * indicated coin receptacle. Used to simulate direct, physical loading of
-     * the receptacle.
-     * 
-     * @param receptacle
-     *            The receptacle where the event occurred.
-     * @param coins
-     *            The coins that were loaded.
-     */
+     	* Announces that the indicated sequence of coins has been added to the
+     	* indicated coin receptacle. Used to simulate direct, physical loading of
+     	* the receptacle.
+     	* 
+     	* @param receptacle
+     	*            The receptacle where the event occurred.
+     	* @param coins
+     	*            The coins that were loaded.
+     	*/
 	@Override
 	public void coinsLoaded(CoinReceptacle receptacle, Coin... coins) {
 		for(Coin coin:coins) {
@@ -103,15 +119,15 @@ public class CoinReceptacleListenerDevice implements CoinReceptacleListener{
 	}
 
 	/**
-     * Announces that the indicated sequence of coins has been removed to the
-     * indicated coin receptacle. Used to simulate direct, physical unloading of
-     * the receptacle.
-     * 
-     * @param receptacle
-     *            The receptacle where the event occurred.
-     * @param coins
-     *            The coins that were unloaded.
-     */
+    	* Announces that the indicated sequence of coins has been removed to the
+     	* indicated coin receptacle. Used to simulate direct, physical unloading of
+     	* the receptacle.
+     	* 
+     	* @param receptacle
+     	*            The receptacle where the event occurred.
+     	* @param coins
+     	*            The coins that were unloaded.
+     	*/
 	@Override
 	public void coinsUnloaded(CoinReceptacle receptacle, Coin... coins) {
 		for(Coin coin: coins) {
