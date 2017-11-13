d776ff020a529592665cedae549e6185841fe5ab
Bigfootmandude <davidjkeizer@gmail.com>
2017-11-12 21:44:29 -0700

    Added Javadoc

--- EventLog.java
+++ EventLog.java
@@ -7,7 +7,17 @@ import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 public class EventLog implements EventLogInterface {
-	PrintWriter writer;
+	
+	
+	private PrintWriter writer;
+	
+		
+	/**
+	* Constructor creates an event log file and writes base contents. 
+	* File name is "WorkLog.txt", formatted in UTF-8
+	* TODO: I suggest that the constructor throws an error rather than catch it so that we dont 
+	* not write the event log in some case.
+	*/
 	public EventLog() {
 		try {
 			writer = new PrintWriter("WorkLog.txt", "UTF-8");
@@ -17,6 +27,12 @@ public class EventLog implements EventLogInterface {
 			e.printStackTrace();
 		}		
 	}
+	
+	/**
+	* Method writs a string to the log on a new line
+	* @param String s, the string to be written to the log
+	* !!TODO!! writer may not exist if the constructor throws an error
+	*/
 	public void writeToLog(String s){
 		timeStamp();
 		writer.println(s + "\n");		
@@ -45,6 +61,10 @@ public class EventLog implements EventLogInterface {
 	}
 	*/
 	
+	/**
+	* Method writes a time stamp in the format yyyy/MM/dd HH:mm:ss to the work log.
+	* !!TODO!! writer may not exist if the constructor throws an error
+	*/
 	public void timeStamp() {
 		DateFormat df = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
 		Date date = new Date();
