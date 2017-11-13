7dc5ce569de8f2111c5178ef49b77cb98606e078
ckbullock410 <ckbullock410@gmail.com>
2017-11-13 13:16:25 -0700

    Add files via upload

--- EventLog.java
+++ EventLog.java
@@ -18,21 +18,17 @@ public class EventLog implements EventLogInterface {
 	* File name is "WorkLog.txt", formatted in UTF-8
 	* TODO: I suggest that the constructor throws an error rather than catch it so that we dont 
 	* not write the event log in some case.
+	 * @throws UnsupportedEncodingException 
+	 * @throws FileNotFoundException 
 	*/
-	public EventLog() {
-		try {
+	public EventLog() throws FileNotFoundException, UnsupportedEncodingException {
 			writer = new PrintWriter("EventLog.txt", "UTF-8");
 			writer.println("DATE/TIME \t\t EVENT");
-		} catch (FileNotFoundException | UnsupportedEncodingException e) {
-			System.out.println("trouble creating EventLog file");
-			e.printStackTrace();
-		}		
 	}
 	
 	/**
 	* Method writs a string to the log on a new line
 	* @param String s, the string to be written to the log
-	* !!TODO!! writer may not exist if the constructor throws an error
 	*/
 	public void writeToLog(String s){
 	/*
@@ -59,7 +55,6 @@ public class EventLog implements EventLogInterface {
 	
 	/**
 	* Method writes a time stamp in the format yyyy/MM/dd HH:mm:ss to the work log.
-	* !!TODO!! writer may not exist if the constructor throws an error
 	*/
 	public void timeStamp() {
 		DateFormat df = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
