ead0021dfb8a1e154339b4b28865846f99738376
Bigfootmandude <davidjkeizer@gmail.com>
2017-11-12 21:25:36 -0700

    Further javadoc and documentation

--- CoinRackListenerDevice.java
+++ CoinRackListenerDevice.java
@@ -15,17 +15,34 @@ public class CoinRackListenerDevice implements CoinRackListener{
 	public boolean racksFull = false;
 	public boolean racksEmpty = false;
 	
+	
+	/**
+	* Constructor creates the listener and assigns a logic to it
+	* @param
+	* @return 
+	*/
 	public CoinRackListenerDevice (VendingLogicInterface logic)
 	{
 		this.logic = logic;
 	}
 	
+	
+	/**
+	* Method enables a specific piece of hardware. 
+	* @param
+	* @return 
+	*/
 	@Override
 	public void enabled(AbstractHardware<? extends AbstractHardwareListener> hardware) {
 	    enabledCount++;
 	    logic.enableHardware(hardware);
 	}
 
+	/**
+	* Method disables a piece of hardware
+	* @param
+	* @return 
+	*/
 	@Override
 	public void disabled(AbstractHardware<? extends AbstractHardwareListener> hardware) {
 	    disabledCount++;
@@ -33,11 +50,11 @@ public class CoinRackListenerDevice implements CoinRackListener{
 	}
 
 	/**
-     * Announces that the indicated coin rack is full of coins.
-     * 
-     * @param rack
-     *            The rack where the event occurred.
-     */
+    	 * Announces that the indicated coin rack is full of coins.
+    	 * 
+   	 * @param rack,  The rack where the event occurred.
+    	 *         
+    	 */
 	@Override
 	public void coinsFull(CoinRack rack) {
 		if(rack.getCapacity()<=rack.size()) {
@@ -49,11 +66,11 @@ public class CoinRackListenerDevice implements CoinRackListener{
 	}
 
 	/**
-     * Announces that the indicated coin rack is empty of coins.
-     * 
-     * @param rack
-     *            The rack where the event occurred.
-     */
+  	* Announces that the indicated coin rack is empty of coins.
+   	* 
+     	* @param rack
+	*           The rack where the event occurred.
+     	*/
 	@Override
 	public void coinsEmpty(CoinRack rack) {
 		if(rack.size() == 0) {
@@ -66,14 +83,14 @@ public class CoinRackListenerDevice implements CoinRackListener{
 	}
 
 	 /**
-     * Announces that the indicated coin has been added to the indicated coin
-     * rack.
-     * 
-     * @param rack
-     *            The rack where the event occurred.
-     * @param coin
-     *            The coin that was added.
-     */
+     	* Announces that the indicated coin has been added to the indicated coin
+     	* rack.
+     	* 
+     	* @param rack
+     	*            The rack where the event occurred.
+     	* @param coin
+     	*            The coin that was added.
+     	*/
 	@Override
 	public void coinAdded(CoinRack rack, Coin coin) {
 		coinValue+=coin.getValue();
@@ -83,13 +100,13 @@ public class CoinRackListenerDevice implements CoinRackListener{
 	}
 
 	/**
-     * Announces that the indicated coin has been added to the indicated coin
-     * rack.
-     * 
-     * @param rack
-     *            The rack where the event occurred.
-     * @param coin
-     *            The coin that was removed.
+     	* Announces that the indicated coin has been added to the indicated coin
+     	* rack.
+     	* 
+     	* @param rack
+     	*            The rack where the event occurred.
+    	 * @param coin
+    	 *            The coin that was removed.
      */
 	@Override
 	public void coinRemoved(CoinRack rack, Coin coin) {
@@ -100,15 +117,15 @@ public class CoinRackListenerDevice implements CoinRackListener{
 	}
 
 	/**
-     * Announces that the indicated sequence of coins has been added to the
-     * indicated coin rack. Used to simulate direct, physical loading of the
-     * rack.
-     * 
-     * @param rack
-     *            The rack where the event occurred.
-     * @param coins
-     *            The coins that were loaded.
-     */
+    	* Announces that the indicated sequence of coins has been added to the
+     	* indicated coin rack. Used to simulate direct, physical loading of the
+     	* rack.
+     	* 
+     	* @param rack
+     	*            The rack where the event occurred.
+    	 * @param coins
+    	 *            The coins that were loaded.
+     	*/
 	@Override
 	public void coinsLoaded(CoinRack rack, Coin... coins) {
 		for(Coin coin : coins) {
@@ -120,15 +137,15 @@ public class CoinRackListenerDevice implements CoinRackListener{
 	}
 
 	/**
-     * Announces that the indicated sequence of coins has been removed to the
-     * indicated coin rack. Used to simulate direct, physical unloading of the
-     * rack.
-     * 
-     * @param rack
-     *            The rack where the event occurred.
-     * @param coins
-     *            The coins that were unloaded.
-     */
+    	* Announces that the indicated sequence of coins has been removed to the
+    	* indicated coin rack. Used to simulate direct, physical unloading of the
+     	* rack.
+     	* 
+    	* @param rack
+    	*            The rack where the event occurred.
+     	* @param coins
+     	*            The coins that were unloaded.
+     	*/
 	@Override
 	public void coinsUnloaded(CoinRack rack, Coin... coins) {
 		for(Coin coin : coins) {
