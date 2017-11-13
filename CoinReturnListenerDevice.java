1cf61beacf10e08025f85e993c05e67009eb465f
Bigfootmandude <davidjkeizer@gmail.com>
2017-11-12 21:32:53 -0700

    Added further documentation

--- CoinReturnListenerDevice.java
+++ CoinReturnListenerDevice.java
@@ -9,23 +9,38 @@ import org.lsmr.vending.hardware.CoinReturnListener;
 public class CoinReturnListenerDevice implements CoinReturnListener {
 
 	private VendingLogicInterface logic;
+	//TODO Make private and create get methods
 	public int enabledCount = 0;
 	public int disabledCount = 0;
 	public int deliveredCoinCount = 0;
 	public int deliveredCoinValue = 0;
 	public boolean returnsfull = false;
 	
+	
+	/**
+	* Constructor creates the listener and assigns a logic to it
+	* @param VendingLogicInterface Logic that the listener interacts with
+	* 
+	*/
 	public CoinReturnListenerDevice(VendingLogicInterface logic)
 	{
 		this.logic = logic;
 	}
 	
+	/**
+	* Method disables a specific peice of hardware
+	* @param AbstractHardware<? extends AbstractHardwareListener> hardware, the piece of hardware to enable
+	*/
 	@Override
 	public void enabled(AbstractHardware<? extends AbstractHardwareListener> hardware) {
 	    enabledCount++;
 	    logic.enableHardware(hardware);
 	}
 
+	/**
+	* Method disables a specific peice of hardware
+	* @param AbstractHardware<? extends AbstractHardwareListener> hardware, the piece of hardware to disable
+	*/
 	@Override
 	public void disabled(AbstractHardware<? extends AbstractHardwareListener> hardware) {
 	    disabledCount++;
@@ -33,12 +48,12 @@ public class CoinReturnListenerDevice implements CoinReturnListener {
 	}
 	
 	/**
-     * Indicates that the coin return will not be able to hold any more
-     * coins.
-     * 
-     * @param coinReturn
-     *            The device on which the event occurred.
-     */
+     	* Indicates that the coin return will not be able to hold any more
+     	* coins.
+     	* 
+     	* @param coinReturn
+     	*            The device on which the event occurred.
+     	*/
 	@Override
 	public void coinsDelivered(CoinReturn coinReturn, Coin[] coins) {
 		for(Coin coin:coins) {
@@ -50,12 +65,12 @@ public class CoinReturnListenerDevice implements CoinReturnListener {
 	}
 	
 	/**
-     * Indicates that the coin return will not be able to hold any more
-     * coins.
-     * 
-     * @param coinReturn
-     *            The device on which the event occurred.
-     */
+     	* Indicates that the coin return will not be able to hold any more
+     	* coins.
+     	* 
+     	* @param coinReturn
+     	*            The device on which the event occurred.
+     	*/
 	@Override
 	public void returnIsFull(CoinReturn coinReturn) {
 		if(coinReturn.getCapacity()<=coinReturn.size()) {
