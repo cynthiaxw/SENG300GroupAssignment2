d118b964180f4e05b49716c401cc834a6b42878f
Bigfootmandude <davidjkeizer@gmail.com>
2017-11-12 21:53:39 -0700

    Added Javadoc

--- PushButtonListenerDevice.java
+++ PushButtonListenerDevice.java
@@ -11,24 +11,39 @@ public class PushButtonListenerDevice implements PushButtonListener {
 
 	private VendingLogicInterface logic;
 	
+	/**
+	* Constructor creates the listener and assigns a logic to it
+	* @param VendingLogicInterface Logic that the listener interacts with
+	* 
+	*/
 	public PushButtonListenerDevice(VendingLogicInterface logic) {
 		this.logic = logic;
 	}
 
 	
+	/**
+	* Method informs the logic interface that a specific button was pressed
+	* @param PushButton button, the button that was pushed
+	*/
 	@Override
 	public void pressed(PushButton button) {
 		logic.determineButtonAction(button);
 	}
 
-
+	/**
+	* Method enables a specific peice of hardware
+	* @param AbstractHardware<? extends AbstractHardwareListener> hardware, the piece of hardware to enable
+	*/
 	@Override
 	public void enabled(AbstractHardware<? extends AbstractHardwareListener> hardware) {
 		logic.enableHardware(hardware);
 		
 	}
 
-
+	/**
+	* Method disables a specific peice of hardware
+	* @param AbstractHardware<? extends AbstractHardwareListener> hardware, the piece of hardware to disable
+	*/
 	@Override
 	public void disabled(AbstractHardware<? extends AbstractHardwareListener> hardware) {
 		logic.disableHardware(hardware);
