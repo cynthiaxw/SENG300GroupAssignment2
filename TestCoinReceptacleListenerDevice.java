073f60a7d82bff7062dfbaee3fa79856886de8d8
Bigfootmandude <davidjkeizer@gmail.com>
2017-11-12 22:05:48 -0700

    Partial Javadoc added

--- TestCoinReceptacleListenerDevice.java
+++ TestCoinReceptacleListenerDevice.java
@@ -2,6 +2,7 @@ package ca.ucalgary.seng300.a2.test;
 
 import static org.junit.Assert.*;
 
+//TODO we really need to find a way to properly structure the file structure
 import org.junit.Test;
 import org.lsmr.vending.Coin;
 import org.lsmr.vending.hardware.AbstractHardware;
@@ -19,25 +20,39 @@ import ca.ucalgary.seng300.a2.EventLogInterface;
 import ca.ucalgary.seng300.a2.VendingLogic;
 import ca.ucalgary.seng300.a2.VendingLogicInterface;
 
+
+//Stub for testing components that use the event logger
 class EventStub implements EventLogInterface {
 
+	/**
+	* Method does nothing. This is to prevent writing a work log when testing
+	*/
 	@Override
 	public void writeToLog(String s) {
 	}
 
+	/**
+	* Method does nothing. This is to prevent writing a work log when testing
+	*/
 	@Override
 	public void timeStamp() {
 	}
 
 }
 
+//Stub for testing the 	Vending logic interface
 class StubLogic implements VendingLogicInterface {
+	
 	public CoinReceptacleListenerDevice dev;
 	public CoinReceptacle hardware;
-	EventStub ev = new EventStub();
+	EventStub ev = new EventStub(); //How does this work not in a constructor or method?
 
 	public boolean enabled = true;
 
+	/**
+	* Creates a logic stub with a coinreceptacle with num capacity
+	* @param int num, the capacity of the coin receptacle
+	*/
 	public StubLogic(int num) {
 		dev = new CoinReceptacleListenerDevice(this);
 		hardware = new CoinReceptacle(num);
@@ -45,17 +60,30 @@ class StubLogic implements VendingLogicInterface {
 
 	}
 
+	/**
+	* getter for event log. 
+	* @return EventLotInterface ev
+	*/
 	@Override
 	public EventLogInterface getEventLog() {
 		return ev;
 	}
 
+	/** 
+	* getter for currency value
+	* @return always returns 0
+	*/
 	@Override
 	public int getCurrencyValue() {
 
 		return 0;
 	}
 
+	/** 
+	* The following methods are implemented, but have no direct use for testing.
+	* For this reason, they all havae no contents.
+	*
+	*/
 	@Override
 	public void welcomeMessageTimer() {
 
@@ -106,17 +134,31 @@ class StubLogic implements VendingLogicInterface {
 
 	}
 
+	
+	/**
+	* Method overide for returning the harware index. Always returns 0
+	* @param AbstractHardware<? extends AbstractHardwareListener> hardware, the hardware to be found
+	* @return returns 0. 
+	*/
 	@Override
 	public int findHardwareIndex(AbstractHardware<? extends AbstractHardwareListener> hardware) {
 
 		return 0;
 	}
 
+	/**
+	* Method disables a specific peice of hardware
+	* @param AbstractHardware<? extends AbstractHardwareListener> hardware, the piece of hardware to disable
+	*/
 	@Override
 	public void disableHardware(AbstractHardware<? extends AbstractHardwareListener> hardware) {
 		enabled = false;
 	}
 
+	/**
+	* Method enables a specific peice of hardware
+	* @param AbstractHardware<? extends AbstractHardwareListener> hardware, the piece of hardware to enable
+	*/
 	@Override
 	public void enableHardware(AbstractHardware<? extends AbstractHardwareListener> hardware) {
 		enabled = true;
@@ -129,9 +171,11 @@ class StubLogic implements VendingLogicInterface {
  * 38% code coverage right now
  *
  */
-
 public class TestCoinReceptacleListenerDevice {
 
+	/**
+	* Tests to that logic is enabled when asked to be
+	*/
 	@Test
 	public void isEnabled() {
 		StubLogic logic = new StubLogic(1);
@@ -139,7 +183,9 @@ public class TestCoinReceptacleListenerDevice {
 		assertTrue(logic.enabled);
 
 	}
-
+	/**
+	* Tests to that logic is disabled when asked to be
+	*/
 	@Test
 	public void isDisabled() {
 		StubLogic logic = new StubLogic(1);
@@ -148,6 +194,10 @@ public class TestCoinReceptacleListenerDevice {
 
 	}
 
+	/**
+	* Tests to that the lgoci handles valid coins
+	* @throws CapacityExceededException, DisabledException
+	*/
 	@Test
 	public void isAccept() throws CapacityExceededException, DisabledException {
 		StubLogic logic = new StubLogic(2);
